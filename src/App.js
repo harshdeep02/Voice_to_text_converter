63ab6dd6f129b02abb2ef1601384006b3701a910
Harsh Deep <harshdeep7887@gmail.com>
2023-07-14 10:59:21 +0530

    initil commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import useClipboard from 'react-use-clipboard';
+import { useState } from 'react';
 function App() {
+
+  const startListening = ()=>{
+    console.log("click")
+    SpeechRecognition.startListening({ continuous: true, language: 'en-IN' }) }
+    const[textCopy, setTextCopy] = useState()
+    const[isCopied, setCopied] = useClipboard(textCopy)
+    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition()
+    if (!browserSupportsSpeechRecognition) {
+      return null
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <div className="container">
+        <h1>Voice to Text Converter</h1>
+        {/* <br /> */}
+        <p>A react hook that converts speech from the mic. to text and makes it available to your React component</p>
+        <p>If you want to copy the text, Firstly click on text then copy button</p>
+        <div className="main-content" onClick={()=>setTextCopy(transcript)}>
+          {transcript}
+          {/* <textarea name="textarea" id="text" cols="30" rows="10"></textarea> */}
+        </div>
+        <div className="btnStyle">
+          <button onClick={setCopied}>{isCopied?'copied':'copy to clipboard'}</button>
+          <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+          <button onClick={startListening}>Start Listening</button>
+        </div>
+      </div>
+    </>
   );
 }
 
